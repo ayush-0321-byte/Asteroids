5bc17f7228b599a71d059c38669ef2d3723b3439
ayush-0321 <cs5200419@iitd.ac.in>
2025-11-08 00:41:12 +0530

    C: Rotate of the Player Working !

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        player.update(dt)
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
